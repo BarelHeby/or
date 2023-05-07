eb9d7938a3a3ca83d980ce30b8f16073d27318dd
BarelHeby <barelheby03@gmail.com>
2023-05-07 19:02:53 +0300

    create better view of projects

--- src/components/User/Projects/Projects.jsx
+++ src/components/User/Projects/Projects.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ProjectsCards from "./ProjectsCard";
 function Projects() {
   const cards = [];
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 6; i++) {
     cards.push(
-      <Col xs={12} sm={6} md={4} lg={3} key={i}>
+      <Col xs={12} sm={6} lg={4} xxl={2} key={i}>
         <ProjectsCards />
       </Col>
     );
   }
   return (
-    <Container className="w-75 ">
+    <Container className="pe-5 ps-5">
       <h1 className="text-center ">פרויקטים</h1>
       <Row className="text-center">{cards}</Row>
     </Container>
--- src/components/User/Projects/ProjectsCard.jsx
+++ src/components/User/Projects/ProjectsCard.jsx
@@ -5,29 +5,40 @@ import Button from "react-bootstrap/Button";
 import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 import kitchen_before_fix from "../../../images/projects/kitchen.jpg";
+import "./carusale.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 function ProjectsCards() {
   return (
     <Card className="mb-3">
-      <Carousel fade pause="hover">
-        <Carousel.Item>
-          <Image src={kitchen} width={300} height={250} />
-        </Carousel.Item>
-        <Carousel.Item>
-          <Image src={kitchen_before_fix} width={300} height={250} />
-        </Carousel.Item>
-      </Carousel>
-      <Card.Body>
-        <Card.Title>שיפוץ מטבח בחולון</Card.Title>
-        <Card.Text>
-          אור נתן חווית שירות מעולה, עניינית ורצינית. מטבח חדש נעשה בתוך 3 ימים
-          בלבד.
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="success" href="#contact">
-          כזה אני רוצה
-        </Button>
-      </Card.Footer>
+      <Container fluid>
+        <Row>
+          <Carousel fade pause="hover" className="">
+            <Carousel.Item>
+              <Image src={kitchen} className="carusaleImg p-0 m-0" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image src={kitchen_before_fix} className="carusaleImg" />
+            </Carousel.Item>
+          </Carousel>
+        </Row>
+        <Row>
+          <Card.Body>
+            <Card.Title>שיפוץ מטבח בחולון</Card.Title>
+            <Card.Text>
+              אור נתן חווית שירות מעולה, עניינית ורצינית. מטבח חדש נעשה בתוך 3
+              ימים בלבד.
+            </Card.Text>
+          </Card.Body>
+        </Row>
+        <Row>
+          <Card.Footer>
+            <Button variant="success" href="#contact">
+              כזה אני רוצה
+            </Button>
+          </Card.Footer>
+        </Row>
+      </Container>
     </Card>
   );
 }
