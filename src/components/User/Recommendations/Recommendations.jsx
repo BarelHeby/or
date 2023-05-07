c40dfde894e3e109ce48c9f852ea954215a8a7f6
BarelHeby <barelheby03@gmail.com>
2023-05-07 18:34:26 +0300

    change column order

--- src/components/User/Recommendations/Recommendations.jsx
+++ src/components/User/Recommendations/Recommendations.jsx
@@ -8,15 +8,15 @@ function Recommendations() {
   const cards = [];
   for (let i = 0; i < 8; i++) {
     cards.push(
-      <Col xs={12} sm={6} md={4} lg={3}>
+      <Col xs={6} sm={6} md={3}>
         <RecomendationCard />
       </Col>
     );
   }
 
   return (
-    <Container className="pe-5 ps-5">
-      <h1 className="text-center ">ביקורות</h1>
+    <Container className="pe-5 ps-5 text-center">
+      <h1 className="mb-3">ביקורות</h1>
       <Row className="align-items-center">
         <Col xs={12} lg={4}>
           <AddRecomandationForm />
