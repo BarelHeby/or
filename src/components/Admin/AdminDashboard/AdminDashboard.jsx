07321977fb81fdf88acd09812487e5e38279fa2d
BarelHeby <barelheby03@gmail.com>
2023-05-08 18:14:04 +0300

    remove unnecceery import

--- src/components/Admin/AdminDashboard/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import AdminNav from "./AdminNav/AdminNav";
-import AdminProjects from "./AdminProjects/AdminProjects";
 function AdminDashboard(props) {
   const navigate = useNavigate();
 
