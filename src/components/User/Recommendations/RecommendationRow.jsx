ac5b71ae78a22ef62dd3471e3c49232b0141e34e
BarelHeby <barelheby03@gmail.com>
2023-05-20 11:37:11 +0300

    avoid refetching several times

--- src/components/User/Recommendations/RecommendationRow.jsx
+++ src/components/User/Recommendations/RecommendationRow.jsx
@@ -6,34 +6,40 @@ import { useGetRecommendations } from "../../../shared/queries";
 
 function RecommendationRow({ numOfRecommendation, setNumOfRecommendations }) {
   const recomendations = useGetRecommendations();
+  let firstTime = true;
   useEffect(() => {
     if (
       !recomendations.isFetching &&
+      recomendations.data !== undefined &&
       recomendations.data.length < numOfRecommendation
     )
       recomendations.refetch();
   }, [recomendations, numOfRecommendation]);
+  useEffect(() => {
+    if (recomendations.data !== undefined)
+      setNumOfRecommendations(recomendations.data.length);
+  }, [setNumOfRecommendations, recomendations]);
   if (recomendations.status === "error")
     return <div>Error has occurred {recomendations.error.message}</div>;
   else if (recomendations.isFetching || recomendations.status === "loading")
     return <div>Loading...</div>;
-  else setNumOfRecommendations(recomendations.data.length);
-  return (
-    <Row className="text-center">
-      {recomendations.data.map((review, key) => {
-        return (
-          <Col key={key} xs={6} sm={6} md={3}>
-            <RecomendationCard
-              name={review.name}
-              review={review.review}
-              rating={review.rating}
-              insert_date={review.insert_date.split("T")[0]}
-            />
-          </Col>
-        );
-      })}
-    </Row>
-  );
+  else
+    return (
+      <Row className="text-center">
+        {recomendations.data.map((review, key) => {
+          return (
+            <Col key={key} xs={6} sm={6} md={3}>
+              <RecomendationCard
+                name={review.name}
+                review={review.review}
+                rating={review.rating}
+                insert_date={review.insert_date.split("T")[0]}
+              />
+            </Col>
+          );
+        })}
+      </Row>
+    );
 }
 
 export default RecommendationRow;
