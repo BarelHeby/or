14ed094791f9863ecec2e3584b5d7af7e20e2e7e
BarelHeby <barelheby03@gmail.com>
2023-05-21 17:31:28 +0300

    osme stylish

--- src/components/User/Phases/Phases.jsx
+++ src/components/User/Phases/Phases.jsx
@@ -25,10 +25,9 @@ function Services() {
   return (
     <div>
       <Form className="">
-        <h1 className="text-center">איך אנחנו עובדים</h1>
+        <h1 className="text-center mb-3">איך אנחנו עובדים</h1>
 
-        <Container fluid className="mt-2 position-relative w-75  ">
-          {/* <Row className="text-center "> */}
+        <Container fluid className=" position-relative w-75  ">
           <Col className="">
             <Row>
               <Col xs={12} md={6}>
@@ -61,42 +60,6 @@ function Services() {
               </Col>
             </Row>
           </Col>
-          {/* <Col>
-            <Image src={room} alt={"logo"} className="img image-fluid" fluid />
-          </Col> */}
-          {/* <Col>
-              <Button className="icon sbtn rounded-circle btn-lg bg-dark-costume border mb-3 ">
-                <span className="fs-1 text-warning-costume">01</span>
-              </Button>
-
-              <h4>{data.phase_1_header}</h4>
-              <p>{data.phase_1}</p>
-            </Col> */}
-          {/* <Col>
-              <Button className="icon btn rounded-circle btn-lg bg-dark-costume border mb-3">
-                <span className="fs-1 text-warning-costume">02</span>
-              </Button>
-
-              <h4>{data.phase_2_header}</h4>
-              <p>{data.phase_2}</p>
-            </Col>
-            <Col>
-              <Button className="icon btn rounded-circle btn-lg bg-dark-costume border mb-3">
-                <span className="fs-1 text-warning-costume">03</span>
-              </Button>
-
-              <h4>{data.phase_3_header}</h4>
-              <p>{data.phase_3}</p>
-            </Col>
-            <Col>
-              <Button className="btn btn-lg rounded-circle   bg-dark-costume border mb-3">
-                <span className="fs-1 text-warning-costume">04</span>
-              </Button>
-
-              <h4>{data.phase_4_header}</h4>
-              <p>{data.phase_4}</p>
-            </Col> */}
-          {/* </Row> */}
         </Container>
       </Form>
     </div>
