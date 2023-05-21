3bfc8334946066c5977a7acb9d4b21f4eb585d47
BarelHeby <barelheby03@gmail.com>
2023-05-21 16:23:56 +0300

    make more buty

--- src/components/User/Services/ServiceCard.jsx
+++ src/components/User/Services/ServiceCard.jsx
@@ -7,16 +7,22 @@ function ServiceCard({ phase, header, text, icon }) {
   const card = useRef();
   const number = useRef();
   const text_contect = useRef();
+  const phase_number = useRef();
   function change_colors(e) {
     card.current.classList.add("bg-dark-costume");
     number.current.classList.remove("bg-dark-costume");
-    number.current.classList.add("bg-white");
+    number.current.classList.add("bg-warning-costume");
     text_contect.current.classList.add("text-white");
+    phase_number.current.classList.add("text-dark-costume");
+    phase_number.current.classList.remove("text-warning-costume");
   }
   function default_colors(e) {
     card.current.classList.remove("bg-dark-costume");
     number.current.classList.add("bg-dark-costume");
     text_contect.current.classList.remove("text-white");
+    phase_number.current.classList.remove("text-dark-costume");
+    phase_number.current.classList.add("text-warning-costume");
+    number.current.classList.remove("bg-warning-costume");
   }
   return (
     <div
@@ -26,18 +32,20 @@ function ServiceCard({ phase, header, text, icon }) {
       onMouseOut={default_colors}
     >
       <div className="row g-0 align-items-center ">
-        <Col xs={3} md={4} lg={3} xxl={2} className="ps-1 pt-1   ">
+        <Col xs={4} lg={3} xxl={2} className="ps-1 pt-1   ">
           <Button
-            className="btn rounded-start bg-dark-costume btn-lg border costume-curser "
+            className="btn rounded-start bg-dark-costume btn-lg border-0 costume-curser "
             ref={number}
           >
-            <span className="fs-1 text-warning-costume ">{phase}</span>
+            <span className="fs-1 text-warning-costume " ref={phase_number}>
+              {phase}
+            </span>
           </Button>
         </Col>
         <div className="col " ref={text_contect}>
           {/* <div className="card-body"> */}
           <h4 className="card-title">{header}</h4>
-          <p className="card-text opacity-50">{text}</p>
+          <p className="card-text opacity-50 pe-1">{text}</p>
           {/* </div> */}
         </div>
       </div>
